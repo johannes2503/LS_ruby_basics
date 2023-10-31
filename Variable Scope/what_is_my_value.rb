31209d41c52d41c5b77f510ec1f96cf64c4da2b6
Cryo79 <johannes2503@gmail.com>
2023-10-31 12:23:22 +0000

    Initial commit

---
+++ Variable Scope/what_is_my_value.rb
@@ -0,0 +1,105 @@
+# Launch School Ruby Basics Excercises
+
+# Part 1
+
+a = 7
+
+def my_value(b)
+  b += 10
+end
+
+my_value(a)
+puts a
+
+# Answer  = 7
+# -Because b is inside the my_value function and not accesible outside it's scope
+
+#---------------------------
+
+# Part 2
+
+a = 7
+
+def my_value(a)
+  a += 10
+end
+
+my_value(a)
+puts a
+
+# Answer  = 
+# -
+
+#---------------------------
+
+# Part 3
+
+
+
+# Answer  = 
+# -
+
+#---------------------------
+
+# Part 4
+
+
+
+# Answer  = 
+# -
+
+#---------------------------
+
+# Part 5
+
+
+
+# Answer  = 
+# -
+
+#---------------------------
+
+# Part 6
+
+
+
+# Answer  = 
+# -
+
+#---------------------------
+
+# Part 7
+
+
+
+# Answer  = 
+# -
+
+#---------------------------
+
+# Part 8
+
+
+
+# Answer  = 
+# -
+
+#---------------------------
+
+# Part 9
+
+
+
+# Answer  = 
+# -
+
+#---------------------------
+
+# Part 10
+
+
+
+# Answer  = 
+# -
+
+#---------------------------
