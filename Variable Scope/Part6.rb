d04f644cf507151164a8d48bc9a823f79b0e423a
Cryo79 <johannes2503@gmail.com>
2023-12-01 22:22:47 +0000

    Part8

--- Variable Scope/Part6.rb
+++ Variable Scope/Part6.rb
@@ -9,5 +9,9 @@ end
 my_value(a)
 puts a
 
-# Answer  = 
-# -
+# Answer  = `my_value': undefined local variable or method `a' for main:Object (NameError)
+
+# - Even though a is defined before my_value is defined, it is not visible inside my_value. Method definitions are self contained with respect to local variables. 
+# Local variables initialized inside the method definition are not visible outside the method definition, and local variables initialized outside the method definition are not visible inside the method definition.
+
+# Note, however, that local variables will be visible (via closures) inside blocks, procs, and lambdas.
